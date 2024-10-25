6fd5ed25b8910b002aa069a45ec78c0be905b056
Kosolpattanadurong Thitiwat <thitiwatipst@icloud.com>
2024-10-25 21:22:20 +0800

    add font family variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        header: ['var(--font-montserrat)'],
+        body: ['var(--font-roboto)'],
+      }
     },
   },
   plugins: [],
