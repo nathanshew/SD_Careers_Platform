300ad33558202698f5907b0caeb62594a78b01dd
NgZiXin <ngzixin02@gmail.com>
2024-10-27 00:01:51 +0800

    Configure settings

--- server/dist/validators/applicants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import * as expressValidator from 'express-validator';
-// const { body, validationResult } = expressValidator;
-// const taskValidationRules = [
-//     body('title').notEmpty().withMessage('Title is required'),
-//     body('description').notEmpty().withMessage('Description is required'),
-//     body('completed').isBoolean().withMessage('Completed must be a boolean'),
-//   ];
